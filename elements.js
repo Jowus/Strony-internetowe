69d6a164863b6ae0237e2caf202f4121052338d8
Jowus <145287807+Jowus@users.noreply.github.com>
2024-09-23 22:07:18 +0200

    guzikiii

---
+++ elements.js
@@ -0,0 +1,5 @@
+const buttonPink = document.querySelector(".pink-button");
+const buttonBlue = document.querySelector(".blue-button");
+const buttonPurple = document.querySelector(".purple-button");
+const buttonYellow = document.querySelector(".yellow-button");
+const body = document.body;
---
+++ main.js
@@ -0,0 +1,15 @@
+buttonPink.addEventListener("click", () => {
+  changeColor("pink");
+});
+
+buttonBlue.addEventListener("click", () => {
+  changeColor("blue");
+});
+
+buttonPurple.addEventListener("click", () => {
+  changeColor("purple");
+});
+
+buttonYellow.addEventListener("click", () => {
+  changeColor("yellow");
+});
